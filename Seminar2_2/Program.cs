0368bb717edba52fa44c94508ce7edc6322d7bad
Aleksandra Obukh <sem0609@mail.ru>
2022-12-03 19:51:11 +0300

    add seminar2_2

---
+++ Seminar2_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет цифру этого числа
+// Например: 456 -> 46
+
+int random = new Random().Next(100, 1000);
+Console.WriteLine(random);
+
+int num1 = random / 100;
+int num2 = random % 100;
+int num3 = num2 % 10;
+
+Console.WriteLine($"{num1}{num3}");
