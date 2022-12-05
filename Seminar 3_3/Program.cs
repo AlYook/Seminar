25fa48d3280b07cf1e7adf754114ea4a1b9d8362
Aleksandra Obukh <sem0609@mail.ru>
2022-12-05 23:01:08 +0300

    add new file

---
+++ Seminar 3_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход
+// координаты двух точек и находит расстояние между
+// ними в 2D пространстве.
+
+Console.WriteLine ("Enter X1 : ");
+int Xa = int.Parse(Console.ReadLine());
+
+Console.WriteLine ("Enter Y1 : ");
+int Ya = int.Parse(Console.ReadLine());
+
+Console.WriteLine ("Enter X2 : ");
+int Xb = int.Parse(Console.ReadLine());
+
+Console.WriteLine ("Enter Y2 : ");
+int Yb = int.Parse(Console.ReadLine());
+
+double AC = (Xb - Xa);
+double BC = (Yb - Ya);
+
+double AB = Math.Pow(AC, 2) + Math.Pow(BC, 2);
+
+double result = Math.Sqrt(AB);
+
+Console.WriteLine(result);
---
+++ Seminar 3_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+// ● 5 -> 1, 4, 9, 16, 25.
+// ● 2 -> 1,4
+
+Console.WriteLine ("Enter num : ");
+int num = int.Parse(Console.ReadLine());
+
+double temp = 1;
+double pow = 0;
+while (temp<=num)
+{
+    pow = Math.Pow(temp, 2);
+    Console.Write(pow);
+    Console.Write(,);
+    temp++;
+}
