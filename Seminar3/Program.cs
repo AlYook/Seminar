6e90f2661a7502437b3adf4b670c8a05b0a98300
Aleksandra Obukh <sem0609@mail.ru>
2022-12-05 19:01:36 +0300

    add file seminar 3

---
+++ Seminar3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход
+// принимает два числа и проверяет, является ли первое
+// число квадратом второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет 
+
+
+Console.WriteLine("Enter number1 ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number2 ");
+int num2 = int.Parse(Console.ReadLine());
+int sqr = num2 * num2;
+
+if (sqr == num1)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
